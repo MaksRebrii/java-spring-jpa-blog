34135ec86c47bbbe289151cb5579372e2e818026
Maksimilian Rebrii <maksim.rebriy@gmail.com>
2022-06-30 20:44:24 +0300

    Completed module 2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,5 +1,6 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
@@ -9,4 +10,6 @@ import java.util.*;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByCategory(Category category);
 }
